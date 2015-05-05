da53077c82e3275e1d8238195523c8a1f046779c
Jongjai Lochman <jongfreedom@gmail.com>
2015-05-05 23:31:16 +0100

    make the function convertFahrenheitToCelsius(temperature) work

--- src/Converter.js
+++ src/Converter.js
@@ -1,4 +1,7 @@
 /** Unit Converter **/
 function convertFahrenheitToCelsius(temperature) {
   //conversion code
+  var farenheit = temperature;
+  var celsius = (farenheit - 32)/1.8;
+  return celsius.toFixed(1);
 }
